748cafca64d0ea2ae740f0303d28054bb54d6ae5
JmirJeff <jmirjeff@gmail.com>
2018-11-17 02:17:49 -0500

    Se creo el entorno

--- main.py
+++ main.py
@@ -24,22 +24,22 @@ class michi():
         self.player2_turn = not(self.player1_turn)
     def verifica_ganador(self):
         for i in range(0,3):
-            if self.states.item(i,0) is self.states.item(i,1) is self.states.item(i,2):
+            if self.states.item(i,0) == self.states.item(i,1) == self.states.item(i,2):
                 if self.states.item(i,0) == 1:
                     return 'player1'
                 else:
                     return 'player2'
-            if self.states.item(0,i) is self.states.item(1,i) is self.states.item(2,i):
+            if self.states.item(0,i) == self.states.item(1,i) == self.states.item(2,i):
                 if self.states.item(0,i) == 1:
                     return 'player1'
                 else:
                     return 'player2'
-        if self.states.item(0,0) is self.states.item(1,1) is self.states.item(2,2):
+        if self.states.item(0,0) == self.states.item(1,1) == self.states.item(2,2):
             if self.states.item(0,0) == 1:
                 return 'player1'
             else:
                 return 'player2'
-        if self.states.item(0,2) is self.states.item(1,1) is self.states.item(2,0):
+        if self.states.item(0,2) == self.states.item(1,1) == self.states.item(2,0):
             if self.states.item(0,0) == 1:
                 return 'player1'
             else:
